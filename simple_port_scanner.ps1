54664cbe70ef1bb232deff93c25edda99f636caa
Kim Troennes <kim@example.com>
2022-10-25 16:33:36 +0200

    Adjust line length

--- simple_port_scanner.ps1
+++ simple_port_scanner.ps1
@@ -3,17 +3,19 @@
 A simple TCP port scanner that does not ping the target
 
 .DESCRIPTION
-Scan one network address for open ports and its hostname. It tries ~60 popular ports by 
-default and skips pinging the target before a scan (similar to -Pn in Nmap)
+Scan one network address for open ports and its hostname. It tries ~60 popular
+ports by default and skips pinging the target before a scan (similar to -Pn in 
+Nmap)
     
 .PARAMETER Address
 Target host address (IP or DNS name)
 
 .PARAMETER Ports
-Ports that should be scanned, default values are 21,22,23,53,69,71,80,81,88,98,110,111,
-113,135,139,143,179,199,389,443,445,465,514,548,587,636,993,995,1025,1026,1080,1433,1521,
-1720,1723,2000,2001,2049,3001,3128,3306,3389,5060,5222,5355,5357,5555,5801,5900,5901,5985,
-5986,6001,6667,6868,7777,7878,8080,8888,10000,27000,27001
+Ports that should be scanned, default values are 21,22,23,53,69,71,80,81,88,98,
+110,111,113,135,139,143,179,199,389,443,445,465,514,548,587,636,993,995,1025,
+1026,1080,1433,1521,1720,1723,2000,2001,2049,3001,3128,3306,3389,5060,5222,
+5355,5357,5555,5801,5900,5901,5985,5986,6001,6667,6868,7777,7878,8080,8888,
+10000,27000,27001
 
 .PARAMETER Timeout
 Time (in milliseconds) before timeout, default is 250
@@ -36,38 +38,40 @@ param(
     $Address,
 
     [int[]]
-    $Ports = @(21,22,23,53,69,71,80,81,88,98,110,111,113,135,139,143,179,199,389,443,445,465, `
-    514,548,587,636,993,995,1025,1026,1080,1433,1521,1720,1723,2000,2001,2049,3001,3128,3306, `
-    3389,5060,5222,5355,5357,5555,5801,5900,5901,5985,5986,6001,6667,6868,7777,7878,8080,8888,`
-    10000,27000,27001),
+    $Ports = @(21, 22, 23, 53, 69, 71, 80, 81, 88, 98, 110, 111, 113, 135, 139, 143, 179, 199, `
+            389, 443, 445, 465, 514, 548, 587, 636, 993, 995, 1025, 1026, 1080, 1433, 1521, 1720, `
+            1723, 2000, 2001, 2049, 3001, 3128, 3306, 3389, 5060, 5222, 5355, 5357, 5555, 5801, `
+            5900, 5901, 5985, 5986, 6001, 6667, 6868, 7777, 7878, 8080, 8888, 10000, 27000, 27001),
 
     [int]
     $Timeout = 250,
 
     [switch]$NoResolve
 )
-if (-not $NoResolve){
+if (-not $NoResolve) {
     $hostname = ([Net.DNS]::GetHostByAddress($Address)).HostName
     Write-Host "Simple port scan results for $Address ($hostname)"
-} else {
+}
+else {
     Write-Host "Simple port scan results for $Address" 
 }
-foreach ($p in $Ports){
+foreach ($p in $Ports) {
     $state = "Closed"
     $client = New-Object System.Net.Sockets.TcpClient
-    $beginConnect = $client.BeginConnect($Address,$p,$null,$null)
-    if ($client.Connected){
+    $beginConnect = $client.BeginConnect($Address, $p, $null, $null)
+    if ($client.Connected) {
         $state = "Open"    
-    } else {
+    }
+    else {
         Start-Sleep -Milli $Timeout
-        if ($client.Connected){
+        if ($client.Connected) {
             $state = "Open"
         }   
     }
     $client.Close()
-    if ($state -eq "Open"){
+    if ($state -eq "Open") {
         [PSCustomObject]@{
-            "Port" = $p
+            "Port"  = $p
             "State" = $state
         }
     }
